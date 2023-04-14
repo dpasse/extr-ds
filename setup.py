0260a309e7359abb5f6f19ca88e8917e5ae2a42c
dpasse <50343386+dpasse@users.noreply.github.com>
2023-04-14 04:53:34 -0500

    setup requirements

--- setup.py
+++ setup.py
@@ -4,13 +4,17 @@ import setuptools
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
+install_requires = """
+extr==0.0.5
+"""
+
 setuptools.setup(
     name='extr-ds',
     version='0.0.1',
     description='',
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
-    install_requires='',
+    install_requires=install_requires.strip(),
     long_description=long_description,
     long_description_content_type='text/markdown',
 )
