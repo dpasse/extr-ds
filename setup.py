abc481a3ce9f559977dbf4a9cda92c8bb13afef5
dpasse <50343386+dpasse@users.noreply.github.com>
2023-04-14 05:06:06 -0500

    clean up

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ import setuptools
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
-install_requires = """
-extr==0.0.5
-"""
-
 setuptools.setup(
     name='extr-ds',
-    version='0.0.1',
+    version='0.0.2',
+    keywords='',
     description='Library to quickly build basic datasets for Named Entity Recognition (NER) and Relation Extraction (RE) Machine Learning tasks.',
     packages=setuptools.find_packages('src'),
     package_dir={'': 'src'},
-    install_requires=install_requires.strip(),
+    install_requires=[
+        'extr==0.0.5'
+    ],
+    url='https://github.com/dpasse/extr-ds',
     long_description=long_description,
     long_description_content_type='text/markdown',
 )
